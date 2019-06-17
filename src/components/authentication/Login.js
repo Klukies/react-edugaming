dd5ec3f4b768a8cad576d03d2555d1f8b3948995
Cornille Lukas <lukas.cornille@student.howest.be>
2019-06-17 17:56:29 +0200

    login stuff

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,31 +1,22 @@
 import React from 'react';
-import styles from './../../assets/css/Authentication.module.css'
-import ModalHeader from './ModalHeader'
+import axios from '../../utils/axios';
+import styles from './../../assets/css/Authentication.module.css';
+import ModalHeader from './ModalHeader';
+import LoginForm from './LoginForm';
 
 export default class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.changeEmail = this.changeEmail.bind(this);
-    this.focusEmail = this.focusEmail.bind(this);
-    this.blur = this.blur.bind(this);
-    this.state = {
-      isEmailFocused: false,
-      email: ''
-    }
-  }
-
-  changeEmail(e) {
-    this.setState({email: e.target.value})
-  }
-
-  focusEmail() {
-    this.setState({isEmailFocused: !this.state.isEmailFocused});
-  }
-
-  blur() {
-    if (this.state.email === '') {
-      this.setState({isEmailFocused: !this.state.isEmailFocused})
-    }
+  loginCallback = (loginData) => {
+    console.log(loginData.email);
+    axios.post('/auth/login', {
+      email: loginData.email,
+      password: loginData.password
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
   }
 
   render() {
@@ -34,29 +25,14 @@ export default class Login extends React.Component {
       <div className={styles.modal} onClick={event => event.stopPropagation()}>
         <div className={styles.closeButton}
         onClick={this.props.closeModal}><b></b><b></b><b></b><b></b></div>
+
         <ModalHeader
         authTitle='Log in'
         subTitle='New to EduGaming?'
         modalLinkText='Sign up for free!'
         modalLink={this.props.showSignup} />
-        <div className={styles.authForm}>
-          <form onSubmit={this.handleSubmit}>
-          <div className='form-field'>
-            <span className={`${styles.error}`}></span>
-            <label className={`${styles.formLabel}
-            ${this.state.isEmailFocused ? styles.focused : 'not'}`}
-            htmlFor='loginEmail'>Email address</label>
-            <input
-            className={styles.formInput}
-            type='text'
-            id='loginEmail'
-            value={this.state.email}
-            onChange={this.changeEmail}
-            onClick={this.focusEmail}
-            onBlur={this.blur}/>
-          </div>
-          </form>
-        </div>
+
+        <LoginForm loginCallback={this.loginCallback}/>
       </div>
     )
   }
---
+++ src/components/authentication/LoginForm.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import styles from './../../assets/css/Authentication.module.css';
+
+const isEmpty = (field) => {
+  return field === '';
+}
+
+const checkEmail = (email) => {
+  return (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email));
+}
+
+const checkPassword = (password) => {
+  return !isEmpty(password);
+}
+
+export default class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.changeEmail = this.changeEmail.bind(this);
+    this.changePassword = this.changePassword.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.focus = this.focus.bind(this);
+    this.blur = this.blur.bind(this);
+    this.state = {
+      email: '',
+      password: '',
+      isEmailFocused: false,
+      isPasswordFocused: false,
+    }
+  }
+
+  changeEmail(e) {
+    this.setState({email: e.target.value})
+    this.focus();
+  }
+
+  changePassword(e) {
+    this.setState({password: e.target.value})
+    this.focus();
+  }
+
+  focus() {
+    if (isEmpty(this.state.email)) {
+      this.setState({isEmailFocused: true});
+    }
+
+    if (isEmpty(this.state.password)) {
+      this.setState({isPasswordFocused: true});
+    }
+  }
+
+  blur() {
+    if (isEmpty(this.state.email)) {
+      this.setState({isEmailFocused: false})
+    }
+
+    if (isEmpty(this.state.password)) {
+      this.setState({isPasswordFocused: false})
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (checkEmail(this.state.email) && checkPassword(this.state.password)) {
+      this.props.loginCallback({
+        email: this.state.email,
+        password: this.state.password
+      })
+    }
+  }
+
+  render () {
+    return(
+      <div className={styles.authForm}>
+        <form onSubmit={this.handleSubmit} autoComplete='off'>
+          <div className='form-field'>
+            <span className={styles.error}></span>
+
+            <label className={`${styles.formLabel}
+            ${this.state.isEmailFocused ? styles.focused : ''}`}
+            htmlFor='loginEmail'>
+            Email address</label>
+
+            <input className={styles.formInput}
+            type='text'
+            id='loginEmail'
+            value={this.state.email}
+            onChange={this.changeEmail}
+            onClick={this.focus}
+            onBlur={this.blur}/>
+          </div>
+          <div className='form-field'>
+            <span className={styles.error}></span>
+
+            <label className={`${styles.formLabel}
+            ${this.state.isPasswordFocused ? styles.focused: ''}`}
+            htmlFor='loginPassword'>
+            Password</label>
+
+            <input className={styles.formInput}
+            type='password'
+            id='loginPassword'
+            value={this.state.password}
+            onChange={this.changePassword}
+            onClick={this.focus}
+            onBlur={this.blur}/>
+          </div>
+          <button type="submit" className="btn submit-btn">Login</button>
+        </form>
+      </div>
+    )
+  }
+}
